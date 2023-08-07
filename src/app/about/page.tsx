97e7af8817f0a4b94e0736e90d6675c25d157579
jyotiraditya <jyotiraditya24@gmail.com>
2023-08-08 02:55:57 +0530

    Learning NextJs13 latest version

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Link from "next/link";
+
+const About = () => {
+  return (
+    <div>
+        <h1>About Page</h1>
+        <Link href="/feature">Link to feature Page</Link>
+    </div>
+  )
+};
+
+export default About;
